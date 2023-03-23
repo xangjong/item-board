1eb100f7a89343043fa25a4cef122400f26c1943
xangjong <csj7080@naver.com>
2023-03-23 19:03:41 +0900

    UPDATE

--- board/src/main/java/com/spring/board/controller/MainController.java
+++ board/src/main/java/com/spring/board/controller/MainController.java
@@ -1,10 +1,7 @@
 package com.spring.board.controller;
 
-import com.spring.board.dto.MemberDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class MainController {
