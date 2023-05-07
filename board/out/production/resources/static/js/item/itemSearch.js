cfdc6beca5357b1b585b8078e76e629131f22057
xangjong <csj7080@naver.com>
2023-05-07 15:16:05 +0900

    UPDATE

--- board/out/production/resources/static/js/item/itemSearch.js
+++ board/out/production/resources/static/js/item/itemSearch.js
@@ -1,49 +1,15 @@
-/**
- * itemSearch.js
- */
+function performSearch(page, type, keyword) {
+    location.href = "boardList?page=" + encodeURIComponent(page) + "&type=" + encodeURIComponent(type) + "&keyword=" + encodeURIComponent(keyword);
+}
 
-$/*(document).ready(function(){
+$(document).ready(function () {
+    $('#searchForm').on('submit', function (event) {
+        event.preventDefault();
 
-    $('#itemSearchFrm').on('submit', function(){
+        let currentPage = 1;
+        let searchType = $('#type').val();
+        let searchKeyword = $('#keyword').val();
 
-        let formData = $(this).serialize();
-
-        console.log("formData : " + formData);
-
-        $.ajax({
-            type:"POST",
-            url:"/itemSearch",
-            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-            dataType:'html',
-            data:formData,
-            success: function(data){ // 컨트롤러에서 반환한 itemList result가 받음
-                // 반환된 결과를 <div>으로 삽입
-                console.log("result : " + data);
-            },
-            error: function(){
-                // 오류있을 경우 수행되는 함수
-                console.log("fail data : " + this.data);
-                alert("전송 실패");
-            }
-        }); // ajax 끝
-    }); // submit 끝
-});  // ready 끝*/
-
-
-
-$(document).on('click', '#btnSearch', function(e){
-
-    e.preventDefault();
-
-    let url = "${pageContext.request.contextPath}/boardList";
-
-    url = url + "?type=" + $('#type').val();
-
-    url = url + "&keyword=" + $('#keyword').val();
-
-    location.href = url;
-
-    console.log(url);
-
+        performSearch(currentPage, searchType, searchKeyword);
+    });
 });
-
